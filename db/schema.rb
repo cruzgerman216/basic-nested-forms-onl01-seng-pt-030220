50a259f67eb7897484c1953db392d45f412f97ae
German Cruz <cruzgerman216@yahoo.com>
2020-08-04 19:03:12 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114112845) do
+ActiveRecord::Schema.define(version: 20160114023723) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street_address_1"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20160114112845) do
     t.string   "address_type"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.integer  "person_id"
   end
 
   create_table "people", force: :cascade do |t|
